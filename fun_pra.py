b7ca68706c4d8cb4d634bf3dca5a81d33be57c16
jishan1223 <141535212+jishan1223@users.noreply.github.com>
2023-08-07 07:48:29 +0545

    Add files via upload

---
+++ fun_pra.py
@@ -0,0 +1,10 @@
+# default parameters
+
+def sub(num1 = 3, num2 = 4):
+    c = num1 - num2
+    return c
+
+# a = int(input("Enter number1: "))
+# b = int(input("Enter number2: "))
+
+print(sub(10, 7))
